ae405cdc93991fb70dcc89cda04192e213a2bce2
nolonger21 <nolonger@qq.com>
2021-01-26 11:10:46 +0800

    fix: a

--- packages/example/index.js
+++ packages/example/index.js
@@ -4,5 +4,5 @@ const merge = require('lodash/merge');
 console.info('example', test);
 test();
 console.info('lodash222-111', merge);
-console.info('lodash222-3333', merge);
+console.info('lodash222-3333-1', merge);
 console.info('lodash222-444', merge);
